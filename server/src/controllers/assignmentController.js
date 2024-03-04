da22ee306e4385d3dd4f0824666d3c35006a68e1
huynhhothoty <20110597@student.hcmute.edu.vn>
2024-03-04 12:29:39 +0700

    update

--- server/src/controllers/assignmentController.js
+++ server/src/controllers/assignmentController.js
@@ -1,8 +1,34 @@
 const { assignmentModel } = require('../models/assignmentModel');
-const { createOne, updateOne, deleteOne, getAll, getOne } = require('./crudController');
+const { CustomError } = require('../utils/CustomError');
+const { createOne, deleteOne, getAll, getOne } = require('./crudController');
 
 const createAssignment = createOne(assignmentModel);
-const updateAssignment = updateOne(assignmentModel);
+const updateAssignment = async (req, res, next) => {
+    try {
+        const updateItem = await assignmentModel.findById(req.params.id);
+        if (!updateItem) return next(new CustomError('No document with this id', 404));
+
+        const updateField = req.body;
+        let newDetail;
+        if (Object.keys(updateField).includes('detail')) {
+            newDetail = updateItem.toObject().detail.map((ele) => {
+                if (updateItem.closeTime < ele.submitTime)
+                    return { ...ele, status: 'late' };
+                return ele;
+            });
+        }
+        updateItem.detail = newDetail;
+        const result = await updateItem.save();
+
+        res.status(200).send({
+            status: 'ok',
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
+        return next(new CustomError(error));
+    }
+};
 const getOneAssignment = getOne(assignmentModel, null);
 const getAllAssignment = getAll(assignmentModel);
 const deleteAssignment = deleteOne(assignmentModel);
--- server/src/models/assignmentModel.js
+++ server/src/models/assignmentModel.js
@@ -20,24 +20,47 @@ const assignmentSchema = new Schema({
     detail: [
         {
             _id: false,
+            submitTime: {
+                type: Date,
+                default: new Date(),
+            },
             status: {
                 type: String,
                 enum: {
-                    values: ['non-submit', 'submitted'],
-                    message: "status are only: 'non-submit', 'submitted'",
+                    values: ['on-time', 'late'],
+                    message: "status are only: 'on-time', 'late'",
                 },
-                default: 'non-submit',
+                default: 'on-time',
             },
             student: {
                 type: Schema.ObjectId,
                 ref: 'User',
             },
+            submitText: {
+                type: String,
+                default: 'Here is my submit content',
+            },
             submitUrl: {
                 type: String,
+                default: 'https://abc.com/image',
             },
         },
     ],
 });
+
+// middleware
+// assignmentSchema.pre('findOneAndUpdate', function (next) {
+//     const update = this.getUpdate();
+//     if (!update.detail) return next(); // Kiểm tra xem trường 'detail' có được cập nhật không
+
+//     const details = update.detail.map((ele) => {
+//         if (this._update.closeTime > ele.submitTime) return { ...ele, status: 'late' };
+//         return ele;
+//     });
+
+//     this.update({}, { $set: { detail: details } }); // Cập nhật lại trường 'detail'
+//     next();
+// });
 
 const assignmentModel = model('Assignment', assignmentSchema);
 module.exports = {
