e1ab3dae4f4165b53fb7951f4b32b61b8a7df7c5
huynhhothoty <20110597@student.hcmute.edu.vn>
2024-03-06 19:08:46 +0700

    update user screen

--- client/screens/Home/Home.jsx
+++ client/screens/Home/Home.jsx
@@ -18,78 +18,82 @@ import LoadingIndicator from '../../util/Loading/LoadingIndicator';
 
 const Home = () => {
     const navigate = useNavigation();
-    const dispatch = useDispatch()
-    const {userInfo,loading} = useSelector(store => store.userSlice)
-    const {todayClassList, allClassList} = useSelector(store => store.classSlice)
+    const dispatch = useDispatch();
+    const { userInfo, loading } = useSelector((store) => store.userSlice);
+    const { todayClassList, allClassList } = useSelector((store) => store.classSlice);
 
-    const [activeNav,setActiveNav] = useState("one")
+    const [activeNav, setActiveNav] = useState('one');
 
     const handleCheckout = async () => {
         const test = await getUserInfo();
         if (test) console.log(test);
     };
-    const handleLogout = async () => {
-        await deleteUserInfo();
-        navigate.navigate('Login');
-    };
 
     const handleChangeTab = (type) => {
-        setActiveNav(type)
-    }
+        setActiveNav(type);
+    };
 
     const renderClassList = () => {
-        if(activeNav === 'one'){
-            if(todayClassList.length > 0){
+        if (activeNav === 'one') {
+            if (todayClassList.length > 0) {
                 return todayClassList.map((classDetail) => {
-                    return <Section
-                        time={`${classDetail.startTime} - ${classDetail.endTime}`}
-                        room={classDetail.room}
-                        isChecked={true}
-                        teacher={'Bảo Nguyên'}
-                        name={classDetail.name}
-                        key={classDetail._id}
-                        classId={classDetail._id}
-                        isAllClass={false}
-                    />
-                })
-            }else return <View style={styles.noClassContainer}>
-                <Text style={styles.noClassText}>No Class for today</Text>
-            </View>
-           
-        }else if(activeNav === 'all'){
-            if(allClassList.length > 0){
+                    return (
+                        <Section
+                            time={`${classDetail.startTime} - ${classDetail.endTime}`}
+                            room={classDetail.room}
+                            isChecked={true}
+                            teacher={'Bảo Nguyên'}
+                            name={classDetail.name}
+                            key={classDetail._id}
+                            classId={classDetail._id}
+                            isAllClass={false}
+                        />
+                    );
+                });
+            } else
+                return (
+                    <View style={styles.noClassContainer}>
+                        <Text style={styles.noClassText}>No Class for today</Text>
+                    </View>
+                );
+        } else if (activeNav === 'all') {
+            if (allClassList.length > 0) {
                 return allClassList.map((classDetail) => {
-                    return <Section
-                        time={`${classDetail.startTime} - ${classDetail.endTime}`}
-                        room={classDetail.room}
-                        isChecked={true}
-                        teacher={classDetail.teacher}
-                        name={classDetail.name}
-                        key={classDetail._id}
-                        classId={classDetail._id}
-                        isAllClass={true}
-                    />
-                })
-            }else return <View style={styles.noClassContainer}>
-                <Text style={styles.noClassText}>You don't have any class</Text>
-            </View>
-           
+                    return (
+                        <Section
+                            time={`${classDetail.startTime} - ${classDetail.endTime}`}
+                            room={classDetail.room}
+                            isChecked={true}
+                            teacher={classDetail.teacher}
+                            name={classDetail.name}
+                            key={classDetail._id}
+                            classId={classDetail._id}
+                            isAllClass={true}
+                        />
+                    );
+                });
+            } else
+                return (
+                    <View style={styles.noClassContainer}>
+                        <Text style={styles.noClassText}>You don't have any class</Text>
+                    </View>
+                );
         }
-    }
+    };
 
     useEffect(() => {
-        dispatch(getTodayClassList())
-        dispatch(getAllClassByTeacherId())
-    },[])
+        dispatch(getTodayClassList());
+        dispatch(getAllClassByTeacherId());
+    }, []);
 
     return (
         <View style={styles.container}>
             <LoadingIndicator loading={loading} />
             <View style={styles.header}>
-                <View style={styles.avatarContainer} >
+                <View style={styles.avatarContainer}>
                     <FontAwesome name='user' size={40} color='#0A426E' />
                 </View>
-                <Text style={styles.name}>{userInfo ? userInfo.name : ""}</Text>
+                <Text style={styles.name}>{userInfo ? userInfo.name : ''}</Text>
             </View>
             <View style={styles.main}>
                 <View
@@ -97,26 +101,54 @@ const Home = () => {
                         marginBottom: 25,
                         marginLeft: 'auto',
                         marginRight: 'auto',
-                        flexDirection:"row",
-
+                        flexDirection: 'row',
                     }}
                 >
-                    <Pressable style={[styles.topNavButton,{borderRightWidth: .5, borderRightColor: "#ccc"},activeNav === "one" ? styles.activeNavButton : {}]} onPress={() => handleChangeTab("one")}>
-                        <Text style={[{ fontSize: 17, fontWeight: 'bold' },activeNav === "one" ? styles.activeNavButtonText : {}]}>Today's Class</Text>
+                    <Pressable
+                        style={[
+                            styles.topNavButton,
+                            { borderRightWidth: 0.5, borderRightColor: '#ccc' },
+                            activeNav === 'one' ? styles.activeNavButton : {},
+                        ]}
+                        onPress={() => handleChangeTab('one')}
+                    >
+                        <Text
+                            style={[
+                                { fontSize: 17, fontWeight: 'bold' },
+                                activeNav === 'one' ? styles.activeNavButtonText : {},
+                            ]}
+                        >
+                            Today's Class
+                        </Text>
                     </Pressable>
-                    <Pressable style={[styles.topNavButton,{borderLeftWidth: .5, borderLeftColor: "#ccc"}, ,activeNav === "all" ? styles.activeNavButton : {}]} onPress={() => handleChangeTab("all")}>
-                        <Text style={[{ fontSize: 17, fontWeight: 'bold' },activeNav === "all" ? styles.activeNavButtonText : {}]}>All Class</Text>
+                    <Pressable
+                        style={[
+                            styles.topNavButton,
+                            { borderLeftWidth: 0.5, borderLeftColor: '#ccc' },
+                            ,
+                            activeNav === 'all' ? styles.activeNavButton : {},
+                        ]}
+                        onPress={() => handleChangeTab('all')}
+                    >
+                        <Text
+                            style={[
+                                { fontSize: 17, fontWeight: 'bold' },
+                                activeNav === 'all' ? styles.activeNavButtonText : {},
+                            ]}
+                        >
+                            All Class
+                        </Text>
                     </Pressable>
                 </View>
                 {renderClassList()}
             </View>
             <View style={styles.buttonContainer}>
-                <Pressable
+                {/* <Pressable
                     onPress={handleLogout}
                     style={[styles.button, styles.mainButton, { marginBottom: 10 }]}
                 >
                     <Text style={styles.mainButtonText}>LOG OUT</Text>
-                </Pressable>
+                </Pressable> */}
                 <Pressable onPress={handleCheckout} style={[styles.button, styles.mainButton]}>
                     <Text style={styles.mainButtonText}>CHECKOUT</Text>
                 </Pressable>
@@ -198,27 +230,27 @@ const styles = StyleSheet.create({
         letterSpacing: 3,
     },
 
-    topNavButton:{
+    topNavButton: {
         flex: 1,
-        alignItems: "center",
+        alignItems: 'center',
         borderBottomWidth: 2,
-        borderBottomColor: "#ccc",
+        borderBottomColor: '#ccc',
         paddingVertical: 12,
     },
     activeNavButton: {
-        borderBottomColor: "#0A426E",
+        borderBottomColor: '#0A426E',
     },
     activeNavButtonText: {
-        color: "#0A426E"
+        color: '#0A426E',
     },
     noClassContainer: {
-        alignItems: "center",
-        justifyContent:"center",
-        paddingTop: 50
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 50,
     },
     noClassText: {
         fontSize: 25,
-        fontWeight: "bold",
-        color: "#ccc"
-    }
+        fontWeight: 'bold',
+        color: '#ccc',
+    },
 });
--- client/screens/User/User.jsx
+++ client/screens/User/User.jsx
@@ -1,43 +1,53 @@
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
+import { deleteUserInfo } from '../../util/storage/userStorage';
+import { useNavigation } from '@react-navigation/native';
 
-function MenuItem({ name, icon, color }) {
+function MenuItem({ name, icon, color, onClick }) {
     return (
-        <View
-            style={{
-                paddingHorizontal: 20,
-                marginVertical: 10,
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-            }}
-        >
+        <Pressable onPress={onClick}>
             <View
                 style={{
-                    height: 50,
-                    width: 50,
-                    backgroundColor: '#f3f2f5',
+                    paddingHorizontal: 20,
+                    marginVertical: 10,
                     display: 'flex',
-                    justifyContent: 'center',
+                    flexDirection: 'row',
                     alignItems: 'center',
-                    borderRadius: 10,
                 }}
             >
-                {icon}
+                <View
+                    style={{
+                        height: 50,
+                        width: 50,
+                        backgroundColor: '#f3f2f5',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: 10,
+                    }}
+                >
+                    {icon}
+                </View>
+                <Text style={{ marginLeft: 20, color: color ? color : 'black' }}>{name}</Text>
+                <MaterialIcons
+                    style={{ marginLeft: 'auto' }}
+                    name='navigate-next'
+                    color={color ? color : 'black'}
+                    size={30}
+                />
             </View>
-            <Text style={{ marginLeft: 20, color: color ? color : 'black' }}>{name}</Text>
-            <MaterialIcons
-                style={{ marginLeft: 'auto' }}
-                name='navigate-next'
-                color={color ? color : 'black'}
-                size={30}
-            />
-        </View>
+        </Pressable>
     );
 }
 
 export default function User() {
+    const navigate = useNavigation();
+
+    const handleLogout = async () => {
+        await deleteUserInfo();
+        navigate.navigate('Login');
+    };
     return (
         <View style={{ flex: 1, backgroundColor: '#f3f2f5' }}>
             <View style={{ display: 'flex', alignItems: 'center', gap: 5, marginTop: 70 }}>
@@ -80,6 +90,7 @@ export default function User() {
                     color='red'
                     name='Logout'
                     icon={<MaterialIcons name='logout' color='rgba(255,0,0,0.6)' size={21} />}
+                    onClick={handleLogout}
                 ></MenuItem>
             </View>
         </View>
